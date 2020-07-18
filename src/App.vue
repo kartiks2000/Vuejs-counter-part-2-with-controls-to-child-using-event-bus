ec0e33e5d1b9b0134402b64af7e738a3da6b8e0e
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-18 16:36:19 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>I am parent component....</h1>
+    <h1>Counter: {{c}}</h1>
+    <button @click="inc">Increment</button>
+    <button @click="dec">Decrement</button>
+    <Child :c="c"></Child>
+  </div>
+</template>
+
+<script>
+import { mybus } from './main';
+import Child from './Components/child';
+export default {
+  components:{
+    Child : Child
+  },
+  data:function(){
+    return{
+      c : 0
+    }
+  },
+  methods:{
+    inc(){
+      this.c += 1;
+    },
+    dec(){
+      this.c -= 1;
+    }
+  },
+  created(){
+    mybus.$on("in",() => this.inc());
+    mybus.$on("de",() => this.dec());
+  }
+}
+</script>
+
+<style scoped>
+  .ch{
+        background-color: rgb(167, 155, 155);
+        padding: 2rem;
+        margin: 5rem;
+    }
+</style>
---
+++ src/Components/child.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="ch">
+      <h1>I am child component...</h1>
+    <h1>Counter: {{c}}</h1>
+    <button @click="incme">Increment</button>
+    <button @click="decme">Decrement</button>
+  </div>
+</template>
+
+<script>
+import { mybus } from '../main';;
+export default {
+    props: ["c"],
+    methods:{
+        incme(){
+            mybus.$emit("in");
+        },
+        decme(){
+            mybus.$emit("de");
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
